449ca09d93b2aeeb15ebe36df91414ff12043025
Eric Collins <ecollins@flatworldknowledge.com>
2012-03-25 19:15:11 -0400

    Update to engine.rb

--- lib/newsie/engine.rb
+++ lib/newsie/engine.rb
@@ -1,11 +1,11 @@
 module Newsie
   class Engine < Rails::Engine
-    initialize "newsie.load_app_instance_data" do |app|
+    initializer "newsie.load_app_instance_data" do |app|
       Newsie.setup do |config|
        config.app_root = app.root
       end
     end
-    initialize "newsie.load_static_assets" do |app|
+    initializer "newsie.load_static_assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
     end
   end
