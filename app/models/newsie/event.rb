9bcf251bf2a547d1dfc1e1c3b2c2bc593f9e1422
Eric Collins <ecollins@flatworldknowledge.com>
2012-03-25 21:23:52 -0400

    Update newsie event to make specs pass

--- app/models/newsie/event.rb
+++ app/models/newsie/event.rb
@@ -1,5 +1,13 @@
 module Newsie
   class Event < ActiveRecord::Base
-    attr_accessible :name, :content, :start_date, :end_date
+    attr_accessible :name, :content, :start_date, :end_date, :dismiss
+    # Search all events to see if any fit within date range
+    def self.extra_extra 
+      self.all.each do |e|
+        if e.start_date <= Time.now()
+          return e.content
+        end
+      end
+    end
   end
 end
