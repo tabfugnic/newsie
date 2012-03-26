4f4ad2b984d54649123ecebf3387897c03bb1144
Eric Collins <ecollins@flatworldknowledge.com>
2012-03-25 21:19:47 -0400

    Updated the specs for event model

--- spec/models/newsie/event_spec.rb
+++ spec/models/newsie/event_spec.rb
@@ -1,23 +1,29 @@
 require 'spec_helper'
 
+require File.join(File.dirname(__FILE__), '..', '..', '..', 'app', 'models', 'newsie', 'event')
+
 module Newsie
   describe Event do
+    before :all do
+      5.times do |i|
+        Newsie::Event.create!( {
+                                 :name	     => "Event #{i}",
+                                 :start_date => Time.now() + (60*60*24*(i-1)), #Start with day before
+                                 :end_dated  => Time.now() + (60*60*24*i),     #Start with current current day
+                                 :content    => "Content #{i}"
+                               } )
+      end
+    end
     let(:event) do
       mock_model Event, :name => "test_event",
       :start_date => Time.now(),
       :end_date => Time.now() + (60*60*24), # next day
       :content => "Random content",
     end
-    let(:event) do
-      mock_model Event, :name => "test_event2",
-      :start_date => Time.now() - (60*60*24), #yesterday
-      :end_date => Time.now() - 1, # one second ago
-      :content => "Random content 2",
-    end
     it { event.should be_valid }
     context "methods" do
       context "extra_extra" do
-        it { extra_extra.should eq("Random content") }
+        it { Event.extra_extra.should eq("Content 0") }
       end
     end
   end
