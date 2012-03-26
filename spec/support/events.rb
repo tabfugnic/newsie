06ccc41c597c4fc455e9eedf0126e9ef27905dd9
Eric Collins <ecollins@flatworldknowledge.com>
2012-03-26 18:05:02 -0400

    Fixed factories for events

--- spec/support/events.rb
+++ spec/support/events.rb
@@ -1,7 +1,9 @@
-Factory.define :event do |e|
-  e.sequence(:name) { |s| "Event #{s}" }
-  e.sequence(:start_date) { |s| DateTime.current + s.days }
-  e.sequence(:end_date) { |s| DateTime.current + (1 + s).days.ago }
-  e.sequence(:content) { |s| "Content #{s}" }
-  e.dismiss false
+FactoryGirl.define do
+  factory :event, :class => Newsie::Event do |e|
+    e.sequence(:name) { |s| "Event #{s}" }
+    e.sequence(:start_date) { |s| DateTime.current - s.days }
+    e.sequence(:end_date) { |s| DateTime.current + (s + 1).days }
+    e.sequence(:content) { |s| "Content #{s}" }
+    e.dismiss false
+  end
 end
