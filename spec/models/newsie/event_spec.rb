3d941a8c528c18eec587599e5a9eb330776d2578
Eric Collins <ecollins@flatworldknowledge.com>
2012-04-01 22:35:06 -0400

    Slight modification to one test and creating a test to see the most recent event

--- spec/models/newsie/event_spec.rb
+++ spec/models/newsie/event_spec.rb
@@ -8,23 +8,32 @@ module Newsie
     it { event.should be_valid }
     context "methods" do
       context "extra_extra" do
-        before(:each) { FactoryGirl.create(:event) }
+        before(:all) { FactoryGirl.create(:event) }
         it "returns any event happening today" do 
           Event.extra_extra.length.should be > 0
         end
         it "does not return events not happening today" do
-          event = FactoryGirl.create(:event, :start_date => DateTime.current - 2.days, :end_date => DateTime.current - 1.day)
+          @event.start_date = DateTime.current - 2.days
+          @event.end_date = DateTime.current - 1.days
+          @event.save!
           Event.extra_extra.should_not include(event)
         end
       end
       context "event_today?" do
-        before(:each) { @event = FactoryGirl.create(:event) }
+        before(:all) { @event = FactoryGirl.create(:event) }
         it { Event.event_today?.should eq(true) }
         it "has no event today" do
           @event.start_date = DateTime.current - 2.days
           @event.end_date = DateTime.current - 1.days
           @event.save!
           Event.event_today?.should eq(false)
+        end
+      end
+      context "just_in" do
+        before(:all) do
+          @event = FactoryGirl.create(:event)
+          FactoryGirl.create(:event, :start_date => Time.now().to_date + 1.day, :end_date => Time.now().to_date + 2.days)
+          FactoryGirl.create(:event, :start_date => Time.now().to_date + 1.day, :end_date => Time.now().to_date + 2.days)
         end
       end
     end
