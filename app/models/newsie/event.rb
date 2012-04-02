b656d8e158b9b2a20c2a3175f1ed5d18bcd44e7b
Eric Collins <ecollins@flatworldknowledge.com>
2012-04-02 11:13:13 -0400

    Moved just_in method to most_current method and added different content for just_in

--- app/models/newsie/event.rb
+++ app/models/newsie/event.rb
@@ -2,13 +2,22 @@ module Newsie
   class Event < ActiveRecord::Base
     attr_accessible :name, :content, :start_date, :end_date, :dismiss
     validates_presence_of :name
-    # Search all events to see if any fit within date range
+    # Search all events to see if any fit within date range of now
     def self.extra_extra
       return where("start_date <= ? AND end_date >= ?", Time.now().to_date, Time.now().to_date)
     end
+    # Check to see if there is an event
     def self.event_today?
       return true if where("start_date <= ? AND end_date >= ?", Time.now().to_date, Time.now().to_date).count > 0
       return false
+    end
+    # Return any event today with without a start_date or end_date
+    def self.just_in
+      return where("start_date = NULL AND end_date = NULL AND created_at >= ?", Time.now().to_date).sort_by(:created_at).last
+    end
+    # Return the most current event from today
+    def self.most_current
+      return self.extra_extra.sort_by(:start_date).last
     end
   end
 end
