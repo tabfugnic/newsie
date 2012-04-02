787f5622afeb4a6471dc56d928a6ddb0861e301e
Eric Collins <ecollins@flatworldknowledge.com>
2012-04-02 11:14:49 -0400

    Added event_controller_spec.rb

---
+++ spec/controllers/event_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+module Newsie
+  describe EventsController do
+    # Potential Ajax call
+    before(:each) { @event = FactoryGirl.create(:event) }
+    context "recent" do
+      before(:all) do
+        FactoryGirl.create(:event, :start_date => Time.now.to_date - 5.days, :end_date => Time.now.to_date - 3.days)
+        FactoryGirl.create(:event, :start_date => Time.now.to_date + 5.days, :end_date => Time.now.to_date - + 7.days)
+        
+      end
+      it "returns the most recent single event without going into the future" do
+        get :recent
+        response.should eq(@event)
+      end
+    end
+  end
+end
