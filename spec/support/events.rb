b5c3f8931f4b3e2e94a76e4c8fa560454690431a
tabfugnic <tabfugnic@gmail.com>
2012-04-03 00:40:09 -0400

    Update to the factory. wanted to make it a bit more predictable

--- spec/support/events.rb
+++ spec/support/events.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :event, :class => Newsie::Event do |e|
     e.sequence(:name) { |s| "Event #{s}" }
-    e.sequence(:start_date) { |s| DateTime.current - s.days }
-    e.sequence(:end_date) { |s| DateTime.current + (s + 1).days }
+    e.sequence(:start_date) { |s| Time.now.to_datetime - 1.days }
+    e.sequence(:end_date) { |s| Time.now.to_datetime + 1.days }
     e.sequence(:content) { |s| "Content #{s}" }
     e.dismiss false
   end
