73777dccdf2041dd07f67e2d342e417145872bed
tabfugnic <tabfugnic@gmail.com>
2012-04-03 11:11:57 -0400

    Update to the event_spec to be even more specific

--- spec/support/events.rb
+++ spec/support/events.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :event, :class => Newsie::Event do |e|
     e.sequence(:name) { |s| "Event #{s}" }
-    e.sequence(:start_date) { |s| Time.now.to_datetime - 1.days }
-    e.sequence(:end_date) { |s| Time.now.to_datetime + 1.days }
+    e.sequence(:start_date) { |s| Time.now.to_datetime - 10.minutes } # Ten minutes earlier from Right now
+    e.sequence(:end_date) { |s| Time.now.to_datetime + 1.days } # about a day from Right now
     e.sequence(:content) { |s| "Content #{s}" }
     e.dismiss false
   end
