ad54852127ebc94fe25d96923aeb46595d02d2d4
tabfugnic <tabfugnic@gmail.com>
2012-04-02 22:52:51 -0400

    Update specs to use standard times and update specs to no longer use :all

--- spec/controllers/event_controller_spec.rb
+++ spec/controllers/event_controller_spec.rb
@@ -4,12 +4,9 @@ module Newsie
     # Potential Ajax call
     before(:each) { @event = FactoryGirl.create(:event) }
     context "recent" do
-      before(:all) do
+      it "returns the most recent single event without going into the future" do
         FactoryGirl.create(:event, :start_date => Time.now.to_date - 5.days, :end_date => Time.now.to_date - 3.days)
         FactoryGirl.create(:event, :start_date => Time.now.to_date + 5.days, :end_date => Time.now.to_date - + 7.days)
-        
-      end
-      it "returns the most recent single event without going into the future" do
         get :recent
         response.should eq(@event)
       end
--- spec/models/newsie/event_spec.rb
+++ spec/models/newsie/event_spec.rb
@@ -8,23 +8,24 @@ module Newsie
     it { event.should be_valid }
     context "methods" do
       context "extra_extra" do
-        before(:all) { @event = FactoryGirl.create(:event) }
+        before(:each) { @event = FactoryGirl.create(:event) }
         it "returns any event happening today" do 
           Event.extra_extra.length.should be > 0
         end
         it "does not return events not happening today" do
-          @event.start_date = DateTime.current - 2.days
-          @event.end_date = DateTime.current - 1.days
+          @event.start_date = Time.now().to_datetime - 2.days
+          @event.end_date = Time.now().to_datetime - 1.days
           @event.save!
+          p @event
           Event.extra_extra.should_not include(event)
         end
       end
       context "event_today?" do
-        before(:all) { @event = FactoryGirl.create(:event) }
+        before(:each) { @event = FactoryGirl.create(:event) }
         it { Event.event_today?.should eq(true) }
         it "has no event today" do
-          @event.start_date = DateTime.current - 2.days
-          @event.end_date = DateTime.current - 1.days
+          @event.start_date = Time.now().to_datetime - 2.days
+          @event.end_date = Time.now().to_datetime - 1.days
           @event.save!
           Event.event_today?.should eq(false)
         end
@@ -37,10 +38,10 @@ module Newsie
         it { Event.just_in.should eq(@event) }
       end
       context "most_current" do
-        before(:all) do
+        before(:each) do
           @event = FactoryGirl.create(:event)
-          FactoryGirl.create(:event, :start_date => Time.now().to_date + 1.day, :end_date => Time.now().to_date + 2.days)
-          FactoryGirl.create(:event, :start_date => Time.now().to_date - 2.day, :end_date => Time.now().to_date + 2.days)
+          FactoryGirl.create(:event, :start_date => Time.now().to_datetime + 1.day, :end_date => Time.now().to_datetime + 2.days)
+          FactoryGirl.create(:event, :start_date => Time.now().to_datetime - 2.day, :end_date => Time.now().to_datetime + 2.days)
         end
         it { Event.most_current.should eq(@event) }
       end
