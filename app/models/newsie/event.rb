e6caf485ceb7db06166b61c7e794f9162d641fb7
tabfugnic <tabfugnic@gmail.com>
2012-04-15 10:44:02 -0700

    Changed the naming convention of the model slightly.  Fixed several specs.

--- app/models/newsie/event.rb
+++ app/models/newsie/event.rb
@@ -3,23 +3,23 @@ module Newsie
 
     attr_accessible :name, :content, :start_date, :end_date, :dismiss
     validates_presence_of :name
-
-    # Search all events to see if any fit within date range of now
+    
+    # Return all events happening right now
     def self.extra_extra
-      return where("start_date <= ? AND end_date >= ?", Time.now().to_date, Time.now().to_date)
+      return where("start_date <= ? AND end_date >= ?", Time.now, Time.now)
     end
 
-    # Check to see if there is an event
-    def self.event_today?
-      return where("start_date <= ? AND end_date >= ?", Time.now().to_date, Time.now().to_date).count > 0
+    # Check to see if any events are happening right now
+    def self.extra_extra?
+      return where("start_date <= ? AND end_date >= ?", Time.now, Time.now).count > 0
     end
 
-    # Return any event today with without a start_date or end_date
+    # Return a one off event, currently denoted by no start date and end date
     def self.just_in
       return where("start_date = NULL AND end_date = NULL AND created_at >= ?", Time.now().to_date).sort_by { |t| t.created_at }.last
     end
 
-    # Return the most current event from today
+    # Return the event that started most recently
     def self.most_current
       return self.extra_extra.sort_by { |t| t.start_date }.last
     end
--- spec/models/newsie/event_spec.rb
+++ spec/models/newsie/event_spec.rb
@@ -7,41 +7,45 @@ module Newsie
     let(:event) { mock_model Event }
     it { event.should be_valid }
     context "methods" do
+
       context "extra_extra" do
         before(:each) { @event = FactoryGirl.create(:event) }
-        it "returns any event happening today" do 
+        it "returns events happening right now" do 
           Event.extra_extra.length.should be > 0
         end
-        it "does not return events not happening today" do
-          @event.start_date = Time.now().to_datetime - 2.days
-          @event.end_date = Time.now().to_datetime - 1.days
+        it "does not have any events to return right now" do
+          @event.start_date = Time.now.to_datetime - 2.days
+          @event.end_date = Time.now.to_datetime - 1.days
           @event.save!
           p @event
           Event.extra_extra.should_not include(event)
         end
       end
-      context "event_today?" do
+
+      context "extra_extra?" do
         before(:each) { @event = FactoryGirl.create(:event) }
-        it { Event.event_today?.should eq(true) }
+        it { Event.extra_extra?.should eq(true) }
         it "has no event today" do
-          @event.start_date = Time.now().to_datetime - 2.days
-          @event.end_date = Time.now().to_datetime - 1.days
+          @event.start_date = Time.now.to_datetime - 2.days
+          @event.end_date = Time.now.to_datetime - 1.days
           @event.save!
-          Event.event_today?.should eq(false)
+          Event.extra_extra?.should eq(false)
         end
       end
+
       context "just_in" do
         before(:each) do
           @event = FactoryGirl.create(:event, :start_date => nil, :end_date => nil )
-          FactoryGirl.create(:event, :start_date => nil, :end_date => nil, :created_at => Time.now().to_datetime - 10.minutes)
+          FactoryGirl.create(:event, :start_date => nil, :end_date => nil, :created_at => Time.now.to_datetime - 10.minutes)
         end
         it { Event.just_in.should eq(@event) }
       end
+
       context "most_current" do
         before(:each) do
           @event = FactoryGirl.create(:event)
-          FactoryGirl.create(:event, :start_date => Time.now().to_datetime + 1.day, :end_date => Time.now().to_datetime + 2.days)
-          FactoryGirl.create(:event, :start_date => Time.now().to_datetime - 2.day, :end_date => Time.now().to_datetime + 2.days)
+          FactoryGirl.create(:event, :start_date => Time.now.to_datetime + 1.day, :end_date => Time.now.to_datetime + 2.days)
+          FactoryGirl.create(:event, :start_date => Time.now.to_datetime - 2.day, :end_date => Time.now.to_datetime + 2.days)
         end
         it { Event.most_current.should eq(@event) }
       end
